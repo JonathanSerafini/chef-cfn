a47560a00602dd82c695ea46435946034a276ecb
Jonathan Serafini <jonathan@lightspeedpos.com>
2015-06-15 08:49:39 -0400

    Bugfix and metadata doc

--- files/default/ohai_plugins/cfn.rb
+++ files/default/ohai_plugins/cfn.rb
@@ -1,4 +1,6 @@
 
+require 'chef/json_compat'
+
 Ohai.plugin(:CFN) do
   provides "cfn/tags",
            "cfn/stack",
@@ -8,92 +10,101 @@ Ohai.plugin(:CFN) do
   depends "ec2"
 
   collect_data do
+    cfn Mash.new
     cfn[:vpc]   = Mash.new
     cfn[:tags]  = Mash.new
     cfn[:stack] = Mash.new
     cfn[:properties]  = Mash.new
-      
+
     begin
       require 'aws-sdk-core'
-      have_gem = true
     rescue LoadError => e
-      Ohai::Log.debug("Cannot load gem: aws-sdk-core")
+      Ohai::Log.error("cfn - cannot load gem: aws-sdk-core")
+      raise
     end
 
-    if have_gem and ec2 = hint?("ec2")
-      region      = ec2[:placement_availability_zone][0...-1]
-      instance_id = ec2[:instance_id]
+    unless hint?("ec2")
+      Ohai::Log.error("cfn - ec2 ohai module failed to load")
+      raise ArgumentError, "ec2 ohai module failed to load"
+    end
 
-      cfn[:vpc][:region_id]  = region
+    region      = ec2[:placement_availability_zone][0...-1]
+    instance_id = ec2[:instance_id]
 
-      # 
-      # Fetch a hash of instance data
-      #
-      begin
-        client = Aws::EC2::Client.new(region: region)
-        instance = client.
-          describe_instances(instance_ids: [aws_instance_id]).
-          reservations.first.
-          instances.first.to_h rescue {}
+    # 
+    # Fetch a hash of instance data
+    #
+    begin
+      client = Aws::EC2::Client.new(region: region)
+      instance = client.
+        describe_instances(instance_ids: [instance_id]).
+        reservations.first.
+        instances.first.to_h
+    rescue Exception => e
+      Ohai::Log.error("cfn: failed to fetch instance: #{e.message}")
+      raise
+    end
+  
+    # Store some vpc related attributes
+    cfn[:vpc][:vpc_id] = instance[:vpc_id]
+    cfn[:vpc][:subnet_id] = instance[:subnet_id]
+    cfn[:vpc][:region_id]  = region
+
+    # SnakeCase attribute keys
+    tags = instance[:tags].map do |hash|
+      hash_key = hash[:key].
+        gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+        gsub(/([a-z\d])([A-Z])/,'\1_\2').
+        tr('-', '_').
+        gsub(/\s/, '_').
+        gsub(/__+/, '_').
+        downcase
+      [hash_key, hash[:value]]
+    end
+
+    # Store instance tags
+    cfn[:tags] = Hash[tags]
+
+    # Store cloudformation stack related attributes
+    cfn[:stack][:stack_id]   = cfn[:tags]["aws:cloudformation:stack_id"]
+    cfn[:stack][:stack_name] = cfn[:tags]["aws:cloudformation:stack_name"]
+    cfn[:stack][:logical_id] = cfn[:tags]["aws:cloudformation:logical_id"]
+    cfn[:stack][:autocaling_name]= cfn[:tags]["aws:autoscaling:group_name"]
+
+    #
+    # Fetch a hash of stack resource metadata
+    #
+    if cfn[:stack][:stack_name]
+      begin 
+        client = Aws::CloudFormation::Client.new(region: region)
+        resource = client.
+          describe_stack_resource(stack_name: cfn[:stack][:stack_name],
+                         logical_resource_id: cfn[:stack][:logical_id]).
+          stack_resource_detail.to_h
       rescue Exception => e
-        Ohai::Log.debug("Failed to fetch instance: #{e.message}")
-        instance = {}
+        Ohai::Log.debug("cfn: failed to fetch stack: #{e.message}")
+        resource = {}
       end
 
-      unless instance.empty?
-        # Store some vpc related attributes
-        cfn[:vpc][:vpc_id] = instance[:vpc_id]
-        cfn[:vpc][:subnet_id] = instance[:subnet_id]
-
-        # Store instance tags
-        cfn[:tags] = Hash[instance[:tags].each do |hash|
-          [hash[:key].downcase, hash[:value]]
-        end]
-
-        # Store cloudformation stack related attributes
-        cfn[:stack][:stack_id]   = cfn[:tags]["aws:cloudformation:stack-id"]
-        cfn[:stack][:stack_name] = cfn[:tags]["aws:cloudformation:stack-name"]
-        cfn[:stack][:logical_id] = cfn[:tags]["aws:cloudformation:logical-id"]
-        cfn[:stack][:autocaling_name]= cfn[:tags]["aws:autoscaling:groupName"]
-      end
-
-      #
-      # Fetch a hash of stack resource metadata
-      #
-      if cfn[:stack][:stack_name]
-        begin 
-          client = Aws::CloudFormation::Client.new(region: region)
-          resource = client.
-            describe_stack_resource(stack_name: cfn[:stack][:stack_name],
-                           logical_resource_id: cfn[:stack][:logical_id]).
-            stack_resource_detail.to_h rescue {}
+      unless resource.empty? or resource[:metadata].nil?
+        begin
+          # Fetch metadata if present
+          metadata = Chef::JSONCompat.
+                      from_json(resource[:metadata], symbolize_keys: true)
+          metadata.each { |k,v| cfn[:properties][k] =v }
         rescue Exception => e
-          Ohai::Log.debug("Failed to fetch stack: #{e.message}")
-          resource = {}
+          Ohai::Log.debug("cfn: failed to parse metadata: #{e.message}")
         end
-
-        unless resource.empty? or resource.metadata.nil?
-          begin
-            # Fetch metadata if present
-            metadata = JSONCompat.
-              from_json(resource.metadata, symbolize_keys: true)
-            metadata.each { |k,v| cfn[:properties][k] =v }
-          rescue Exception => e
-            Ohai::Log.debug("Failed to parse metadata: #{e.message}")
-          end
-        else
-          Ohai::Log.debug("No metadata found: #{cfn[:stack][:logical_id]}")
-        end
+      else
+        Ohai::Log.debug("cfn: no metadata found: #{cfn[:stack][:logical_id]}")
       end
-
-      # 
-      # Add additional properties from hints file
-      #
-      cfn_properties_hints = hint?("cfn_properties") || {}
-      cfn_properties_hints.each { |k,v| cfn[:properties][k] = v }
-    else
-      Ohai::Log.debug("Could not fetch instance details")
     end
+
+    # 
+    # Add additional properties from hints file
+    #
+    cfn_properties_hints = hint?("cfn_properties") || {}
+    cfn_properties_hints.each { |k,v| cfn[:properties][k] = v }
   end
 end
 
--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,100 @@
 name             'chef_cfn'
-maintainer       'Lightspeed'
-maintainer_email 'jonathan@lightspeedpos.com'
+maintainer       'Jonathan Serafini'
+maintainer_email 'jonathan@serafini.ca'
 license          'Apache 2.0'
-description      'Installs/Configures chef_cfn'
-long_description 'Installs/Configures chef_cfn'
+description      'Chef integration with AWS cloudformation'
+long_description 'Chef integration with AWS cloudformation'
 version          '0.1.0'
 
 depends "chef_handler"
 depends "ohai"
+
+# 
+# Attributes
+#
+grouping "cfn",
+  title: "Cloudformation",
+  description: "Cloudformation namespace"
+
+grouping "cfn/properties",
+  title: "Cloudformation Properties",
+  description: "Cloudformation metadata properties merged with cfn hint"
+
+grouping "cfn/stack",
+  title: "Cloudformation Stack",
+  description: "Cloudformation Stack ohai namespace"
+
+attribute "cfn/stack/autoscaling_name",
+  display_name: "Autoscaling group name",
+  description: "Name of the autoscaling group that spawn the instance",
+  default: "ohai"
+
+attribute "cfn/stack/logical_id",
+  display_name: "Cloudformation stack logical id",
+  description: "Cloudformation stack logical id",
+  default: "ohai"
+
+attribute "cfn/stack/stack_id",
+  display_name: "Cloudformation stack id",
+  description: "Cloudformation stack id",
+  default: "ohai"
+
+attribute "cfn/stack/stack_name",
+  display_name: "Cloudformation stack name",
+  description: "Cloudformation stack name",
+  default: "ohai"
+
+grouping "cfn/tags",
+  title: "Cloudformation Tags",
+  description: "Cloudformation Tags ohai namespace"
+
+attribute "cfn/tools/hup/interval",
+  title: "Scan interval",
+  description: "cfn-hup will scan for metadata changes every N seconds",
+  default: 10
+
+attribute "cfn/tools/hup/verbose",
+  title: "cfn-hup verbosity",
+  description: "Should cfn-hup provide verbose output",
+  default: false
+
+attribute "cfn/tools/url",
+  title: "Cloudformation init tools url",
+  description: "Tarball url for cfn-init installation"
+
+grouping "cfn/vpc",
+  title: "Cloudformation VPC",
+  description: "Cloudformation VPC ohai namespace"
+
+attribute "cfn/vpc/region_id",
+  display_name: "Aws Region id",
+  description: "Aws region the instance belongs to",
+  default: "ohai"
+
+attribute "cfn/vpc/subnet_id",
+  display_name: "Aws Subnet id",
+  description: "Aws subnet the instance belongs to",
+  default: "ohai"
+
+attribute "cfn/vpc/vpc_id",
+  display_name: "Aws VPC id",
+  description: "Aws vpc the instance belongs to",
+  default: "ohai"
+
+#
+# Recipes
+#
+
+recipe "chef_cfn::default",   "Installs dependencies"
+recipe "chef_cfn::knife",     "(optional) Provides a basic knife.rb"
+recipe "chef_cfn::ohai",      "Installs the ohai[cfn] plugin"
+recipe "chef_cfn::handler",   "Installs a handler to signal cloudformation"
+recipe "chef_cfn::mounts",    "Mounts cloudformation defined volumes"
+recipe "chef_cfn::tools",     "Installs cloudformation cfn-init tools"
+recipe "chef_cfn::shutdown",  "Installs a service which will delete the node"
+
+#
+# Resources
+#
+provides "chef_cfn::chef_cfn_signal"
 
