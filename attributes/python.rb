217f0622dbc961a6c73347291d9b87fbbf284e0f
Michiel Rensen <michiel.rensen@lightspeedhq.com>
2021-04-14 10:01:52 +0200

    Installing python using poise-python with the v2 URL for get-pip

---
+++ attributes/python.rb
@@ -0,0 +1,6 @@
+# Default python options
+#
+default['poise-python']['options']['get_pip_url'] = 'https://bootstrap.pypa.io/pip/2.7/get-pip.py'
+default['poise-python']['options']['setuptools_version'] = false
+default['poise-python']['options']['wheel_version'] = false
+default['poise-python']['options']['virtualenv_version'] = false
--- recipes/python.rb
+++ recipes/python.rb
@@ -1,7 +1,3 @@
 # Install common python dependencies
 #
-node.default['poise-python']['options']['get_pip_url'] = 'https://bootstrap.pypa.io/pip/2.7/get-pip.py'
-node.default['poise-python']['options']['pip_version'] = '18.0'
-node.default['poise-python']['install_python2'] = true
-node.default['poise-python']['install_python3'] = false
 include_recipe 'poise-python'
