7eb4e6ce853365b1f7e82c1e6ce370e346568ce4
Hector Perez <hector.perez@lightspeedhq.com>
2022-07-19 13:45:43 +0200

    Added certs

---
+++ recipes/certs.rb
@@ -0,0 +1,8 @@
+sudo 
+
+execute "install latest certs" do
+    command "curl -k https://curl.se/ca/cacert-2022-07-19.pem -o /etc/ssl/certs/ca-certificates.crt ;
+        # cp /etc/ssl/certs/ca-certificates.crt /opt/chef/embedded/ssl/certs/cacert.pem ;
+        # cp /etc/ssl/certs/ca-certificates.crt /usr/lib/ssl/cert.pem"
+    elevated "true"
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,6 @@
 component = node['cfn']['recipes']
 
+include_recipe 'chef_cfn::certs'
 include_recipe 'chef_cfn::python'
 include_recipe 'chef_cfn::ohai'       if component['ohai']
 include_recipe 'chef_cfn::handler'    if component['handler']
