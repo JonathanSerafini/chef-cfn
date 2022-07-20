bdb753d4aa56210f867ebe423fa79870b10ca62d
Hector Perez <hector.perez@lightspeedhq.com>
2022-07-20 13:40:52 +0200

    Added PyYAML 3.12

--- recipes/certs.rb
+++ recipes/certs.rb
@@ -1,8 +1,6 @@
-sudo 
-
 execute "install latest certs" do
     command "curl -k https://curl.se/ca/cacert-2022-07-19.pem -o /etc/ssl/certs/ca-certificates.crt ;
-        # cp /etc/ssl/certs/ca-certificates.crt /opt/chef/embedded/ssl/certs/cacert.pem ;
-        # cp /etc/ssl/certs/ca-certificates.crt /usr/lib/ssl/cert.pem"
-    elevated "true"
+        cp /etc/ssl/certs/ca-certificates.crt /opt/chef/embedded/ssl/certs/cacert.pem ;
+        cp /etc/ssl/certs/ca-certificates.crt /usr/lib/ssl/cert.pem"
+    user "root"
 end
