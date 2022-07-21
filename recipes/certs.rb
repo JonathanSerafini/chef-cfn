24c6b745adc06b4d8389538f901e84b3f6162862
Hector Perez <hector.perez@lightspeedhq.com>
2022-07-21 11:30:59 +0200

    Removed certs

--- recipes/certs.rb
+++
@@ -1,6 +0,0 @@
-execute "install latest certs" do
-    command "curl -k https://curl.se/ca/cacert-2022-07-19.pem -o /etc/ssl/certs/ca-certificates.crt ;
-        cp /etc/ssl/certs/ca-certificates.crt /opt/chef/embedded/ssl/certs/cacert.pem ;
-        cp /etc/ssl/certs/ca-certificates.crt /usr/lib/ssl/cert.pem"
-    user "root"
-end
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,5 @@
 component = node['cfn']['recipes']
 
-include_recipe 'chef_cfn::certs'
 include_recipe 'chef_cfn::python'
 include_recipe 'chef_cfn::ohai'       if component['ohai']
 include_recipe 'chef_cfn::handler'    if component['handler']
