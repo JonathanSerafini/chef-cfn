1ec8047772a95934eeaefe4c3719efc2ec6bff1b
Hector Perez <hector.perez@lightspeedhq.com>
2022-07-28 16:06:36 +0200

    Fixed certs install

--- recipes/certs.rb
+++ recipes/certs.rb
@@ -1,8 +1,5 @@
-sudo 
-
 execute "install latest certs" do
     command "curl -k https://curl.se/ca/cacert-2022-07-19.pem -o /etc/ssl/certs/ca-certificates.crt ;
-        # cp /etc/ssl/certs/ca-certificates.crt /opt/chef/embedded/ssl/certs/cacert.pem ;
-        # cp /etc/ssl/certs/ca-certificates.crt /usr/lib/ssl/cert.pem"
-    elevated "true"
+        cp /etc/ssl/certs/ca-certificates.crt /opt/chef/embedded/ssl/certs/cacert.pem ;
+        cp /etc/ssl/certs/ca-certificates.crt /usr/lib/ssl/cert.pem"
 end
